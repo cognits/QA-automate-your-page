4999d5b78c92cfc37ce5acfd205c510ca1602e86
MRodriguezz <marcos.roag@gmail.com>
2015-07-06 13:55:30 -0600

    Archivo Final

--- html_generator.py
+++ html_generator.py
@@ -1,87 +1,65 @@
-
  # -*- coding: utf-8 -*-
-notes = [['Basics', '''The internet is full of lots of servers hosting web pages which are basically HTML (HyperText Markup Language) documents my browser can interpet and present to me.
- HTML is made up of Tags, Attributes and Elements. Some tags are inline and some tags are block, the latter of which essentially puts a 'box' around the content allowing for additional
- specific formatting. DIV is an example of a block element, whereas SPAN is an example of an inline element, but both are container elements. Usually an opening TAGs will need a closing
- TAG except in the case of void tags such as "img" or "br", void tags do not require a corresponding closing tag. 
+ #arrays store data displayed when generating code
+ #and showing console information
+notes = [['Python and Computer Science Intro', '''Sometimes it can be 
+difficult to know where to start with a computer program as there is 
+so much to complete. First you must break down the problem into smaller 
+pieces and tackle each as their own entity. With code you can precicely 
+describe the steps required to solve the problem and these steps can be
+completed by a computer! A computer is just a machine that can be 
+programmed, and it can do almost anything so long as a program is 
+written that can tell it what to do!
+ 
+At first it may seem strange that we need new languanges in order to 
+tell a computer what do to, but this is due to a number of inherant 
+problems with languages such as English; primarily they are too ambiguous 
+and verbose! So there are a number of programming languages out there to 
+learn that will enable a person to write instructions, or a "program", 
+that will tell a computer what to do.'''],
 
-HTML Tags can reference text, images, audio, video or even just plain old text - all filetypes can exist on the internet! 
+['Debugging', '''When using example code always test it to make sure
+it works properly to begin with. If you have problems and need to debug 
+your own code, just comment it out to begin with so when you get it 
+working you will be able to see where you went wrong to begin with and 
+learn from it. Sometimes is is also helpful when debugging code to 
+introduce print statements so that you can see what variables are set 
+to at various points throughout your code. This can help identify 
+where the problem is.
 
-Two of the most important things to remember from the first lesson
-1. Computers are Stupid! Computers need very specific instructions and will perform exactly what you ask for, they cannot make intuitive guesses (Well, maybe with AI but that is waaaay out
-of scope!)
-2. It's ok to forget! It is not possible for everyone to remember everything, but what is important is to have knowledge of the existence of the subject matter so you know where to start
-looking. 
-'''],
-        ['HTML' , '''HTML is at the heart of the internet and can be thought of in 4 different elements 
-1.Text Content - This is what you SEE 
-2. Markup - What it LOOKS LIKE 
-3. Refrences - Links to images, videos or other documents. 
-4. Links to other HTML Pages! 
-•Web pages are made up of rectangles, but remember that not all are visible.
-•HTML Controls the structure of a web page whereas CSS controls the style of the page and both are 'languages'.
-•When programmers talk about the DOM they are talking about the structure of the page
-•Browsers take the HTML Tag and turn them into elements of a tree
-•Boxes are defined with DIV tags
-•Use CLASS attributes to define styles
-•HTML tags can belong to multiple classes
-•Use SPAN and DIV to group related content together
-'''],
-        ['Boxes', '''When creating a structured document you must first divide everything up into boxes and define those boxes with DIV tags. You can also give each element a CLASS
-attribute so that you can easily apply styles afterwards. Tag's can belong to multiple Classes. 
-
-Boxes have 4 elements: 
-1.Margin
-2.Border
-3.Padding
-4.Content
-
- The code box-sizing:border-box will allow you to set the size of the total box including the border and margin, which makes it much easier to determine the actual size of the box rather
- than just the content. In addition, you can choose to set the size in terms of percentage of screen rather than defined pixel count!'''],
-        ['CSS', '''CSS (Cascading Style Sheets) allows you to add style to the website or text and correctly structured will allow programmers to avoid repitition. We do not want to have
-to repeat the same style code over and over again since it wastes time, is difficult to manage and susceptible to errors. What we want is for the code to cascade through the site and only
-well structured code will allow that!
-
- There is a CSS library available to use by following this link.
-
- CSS can be stored either in the HTML or within a separate CSS file of it's own, which is usually the better option for all but the most basic of sites. If you do have a separate CSS file
- you must locate this within the same folder as the HTML file referencing it. To reference it,
- in the HEAD tag include the stylesheet "link" reference line. (Type link and hit tab!)''']
-
-         ]
-
-
-
-def generate_concept_HTML(concept_title, concept_description):
+'Debuigging Strategy:'
+1) Examine error messages when programs crash
+2) Work from example code
+3) Make sure examples work
+4) Check (print) intermediate results
+5) Keep and compare old versions''']]
+#the functions used in the code show what is the 
+#title, the Descripsion and content stored in the arrays, so console display content
+def generate_information_HTML(information_title, information_description):
     html_text_1 = '''
 <div class="concept">
     <div class="concept-title">
-        ''' + concept_title
+        ''' + information_title
     html_text_2 = '''
     </div>
     <div class="concept-description">
-        ''' + concept_description
+        ''' + information_description
     html_text_3 = '''
     </div>
 </div>'''
-    
-    full_html_text = html_text_1 + html_text_2 + html_text_3
-    return full_html_text
+    all_html_text = html_text_1 + html_text_2 + html_text_3
+    return all_html_text
 
-
-
-def return_HTML_Text(notes):
+def return_HTML_Information(notes):
     all_html = ''
     num = 0
     for item in notes:
-        concept = notes[num]
-        concept_html = generate_concept_HTML(concept[0], concept[1])
-        all_html = all_html + concept_html
+        information = notes[num]
+        information_html = generate_information_HTML(information[0], information[1])
+        all_html = all_html + information_html
         num = num+1
     return all_html
 
-
-print return_HTML_Text(notes)
+print return_HTML_Information(notes)
 
 
 
